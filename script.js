7d9503a1a079778d1acb72be29ed2024999e147a
sirazulsihab <smsisihab@gmail.com>
2021-09-03 23:06:47 +0600

    icon and codition added

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ const loadWeather = () => {
     const inputCity = document.getElementById('input-city');
     const cityValue = inputCity.value;
     const apiKey = '3b74c975d00ffb895885fee101e99945';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
     .then(res => res.json())
     .then(data => displayWeatherStatus(data))
 }
-
+const setValue = (id, value) => {
+    document.getElementById(id).innerText = value;
+}
 const displayWeatherStatus = weather => {
-    console.log(weather.main.temp);
-    console.log(weather);
-
-    const cityName = document.getElementById('city-name');
-    cityName.innerText = weather.name;
-    const temp = document.getElementById('temp');
-    temp.innerText = weather.main.temp
+    // set city Name
+    setValue('city-name' , weather.name);
+    // set temp
+    setValue('temp', weather.main.temp)
+    // set weather condition
+    setValue('condition', weather.weather[0].main);
+    const url = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('icon');
+    imgIcon.setAttribute('src', url);
 }
 
 const searchBtn = document.getElementById('searchBtn');
